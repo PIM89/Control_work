f490d327a32dd5d7060aa3f155158640910d1f09
P.I.M <z55666@yandex.ru>
2022-09-05 22:26:51 +0300

    add program.cs

--- program/Program.cs
+++ program/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите данные в массив через запятую: ");
+string text = Console.ReadLine();
+string[] array = text.Split(',', ' ');
+string temp = string.Empty;
+
+string[] SortElem(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length > 0 && array[i].Length <= 3) temp += $"{array[i]}, ";
+    }
+    string[] new_array = temp.Split(',');
+    return new_array;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++) Console.Write($"{array[i]},");
+}
+
+PrintArray(SortElem(array));
